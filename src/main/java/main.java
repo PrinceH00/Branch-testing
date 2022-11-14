61777f3eb36831b7ead0d9d0615704a7374a554c
PrinceH00 <hamz0230@stud.kea.dk>
2022-11-14 09:34:01 +0100

    Main kalsse oprettet. oprettet objecter af klasserne Dog og Cat

---
+++ src/main/java/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+       Dog dog = new Dog();
+       Cat cat = new Cat();
+
+       dog.makeSound();
+       cat.makeSound();
+    }
+}
